3969d310527517fd189c184ae791176f5a8bda27
Thomas Hillebrandt <thillebrandt@marginplug.in>
2024-12-29 17:45:37 -0600

    Tests

--- rules/html-id-case.test.js
+++ rules/html-id-case.test.js
@@ -21,142 +21,216 @@ describe("html-id-case", () => {
 
     ruleTester.run("kebab-case", rule, {
       valid: [
-        // {
-        //   code: '<div id="kebab-case"></div>',
-        //   options,
-        // },
-        // {
-        //   code: '<div id="kebab-case-{{ withInterpolation }}-and-{{more}}"></div>',
-        //   options,
-        // },
-        // {
-        //   code: '<div id="kabob-case-{{ withInterpolation }}-{{andMore}}"></div>',
-        //   options,
-        // },
-        // {
-        //   code: '<div id="kerbab-case-{{ withInterpolation+andMore+evenStill }}"></div>',
-        //   options,
-        // },
-        // {
-        //   code: '<div id="ignored Case {{ withInterpolation }}"></div>',
-        //   options: [{ case: "kebab", ignoreNgInterpolation: true }],
-        // },
+        {
+          code: "<div></div>",
+          options,
+        },
+        {
+          code: '<div [id]="myIdVariable"></div>',
+          options,
+        },
+        {
+          code: '<div id="kebab-case"></div>',
+          options,
+        },
+        {
+          code: '<div id="kebab-case-{{ withInterpolation }}-and-{{more}}"></div>',
+          options,
+        },
+        {
+          code: '<div id="kebab-case-{{ withInterpolation }}-{{andMore}}"></div>',
+          options,
+        },
+        {
+          code: '<div id="kebab-case-{{ withInterpolation+andMore+evenStill }}"></div>',
+          options,
+        },
+        {
+          code: '<div id="ignored Case {{ withInterpolation }}"></div>',
+          options: [{ case: "kebab", ignoreNgInterpolation: true }],
+        },
       ],
       invalid: [
-        // {
-        //   code: '<div id="kebab-case {{ withInterpolation }}"></div>',
-        //   output: '<div id="kebab-case-{{ withInterpolation }}"></div>',
-        //   errors: [
-        //     { message: "Template id attributes should be in kebab-case." },
-        //   ],
-        // },
-  // {
-  //   code: '<div id="kebab-case {{ withInterpolation+andMore }}"></div>',
-  //   output:
-  //     '<div id="kebab-case-{{ withInterpolation + andMore }}"></div>',
-  //   errors: [
-  //     { message: "Template id attributes should be in kebab-case." },
-  //   ],
-  // },
-        // {
-        //   code: '<div id="snake_case"></div>',
-        //   output: '<div id="snake-case"></div>',
-        //   errors: [
-        //     { message: "Template id attributes should be in kebab-case." },
-        //   ],
-        // },
-        // {
-        //   code: '<div id="snake_case_{{ withInterpolation }}"></div>',
-        //   output: '<div id="snake-case-{{ withInterpolation }}"></div>',
-        //   errors: [
-        //     { message: "Template id attributes should be in kebab-case." },
-        //   ],
-        // },
-        // {
-        //   code: '<div id="snake_case_{{ myObject.withInterpolation }}"></div>',
-        //   output: '<div id="snake-case-{{ myObject.withInterpolation }}"></div>',
-        //   errors: [
-        //     { message: "Template id attributes should be in kebab-case." },
-        //   ],
-        // },
-        //       {
-        //   code: '<div id="snake_case_{{ this.withInterpolation }}"></div>',
-        //   output: '<div id="snake-case-{{ this.withInterpolation }}"></div>',
-        //   errors: [
-        //     { message: "Template id attributes should be in kebab-case." },
-        //   ],
-        // },
-        // {
-        //   code: '<div id="snake_case_{{ this.myObject.withInterpolation }}"></div>',
-        //   output: '<div id="snake-case-{{ this.myObject.withInterpolation }}"></div>',
-        //   errors: [
-        //     { message: "Template id attributes should be in kebab-case." },
-        //   ],
-        // },
-        // {
-        //   code: '<div id="snake_case_{{ withMethod() }}"></div>',
-        //   output: '<div id="snake-case-{{ withMethod() }}"></div>',
-        //   errors: [
-        //     { message: "Template id attributes should be in kebab-case." },
-        //   ],
-        // },
         {
-          code: `<div id="snake_case_{{ withVar + 'and_string' + withMethod(someArgument, this.someOther(index)) + { magna: 'carta', magnum: { opus: 1 } } + 512 + [1, 2, 3] + null + false + undefined }}"></div>`,
-          output: `<div id="snake-case-{{ withVar + 'and_string' + withMethod(someArgument, this.someOther(index)) + { magna: 'carta', magnum: { opus: 1 } } + 512 + [1, 2, 3] + null + false + undefined }}"></div>`,
+          code: '<div id=" kebab-case"></div>',
+          output: '<div id="kebab-case"></div>',
+          options,
           errors: [
             { message: "Template id attributes should be in kebab-case." },
           ],
         },
-                    // {
-  //   code: '<div id="snake_case_{{ withInterpolation + andMore }}"></div>',
-  //   output:
-  //     '<div id="snake-case-{{ withInterpolation + andMore }}"></div>',
-  //   errors: [
-  //     { message: "Template id attributes should be in kebab-case." },
-  //   ],
-  // },
-  // {
-  //   code: '<div id="camelCase"></div>',
-  //   output: '<div id="camel-case"></div>',
-  //   errors: [
-  //     { message: "Template id attributes should be in kebab-case." },
-  //   ],
-  // },
-  // {
-  //   code: '<div id="camelCase{{ withInterpolation }}"></div>',
-  //   output: '<div id="camel-case-{{ withInterpolation }}"></div>',
-  //   errors: [
-  //     { message: "Template id attributes should be in kebab-case." },
-  //   ],
-  // },
-  // {
-  //   code: '<div id="PascalCase"></div>',
-  //   output: '<div id="pascal-case"></div>',
-  //   errors: [
-  //     { message: "Template id attributes should be in kebab-case." },
-  //   ],
-  // },
-  // {
-  //   code: '<div id="PascalCase{{ withInterpolation }}"></div>',
-  //   output: '<div id="pascal-case-{{ withInterpolation }}"></div>',
-  //   errors: [
-  //     { message: "Template id attributes should be in kebab-case." },
-  //   ],
-  // },
-  // {
-  //   code: '<div id="Mixed case"></div>',
-  //   output: '<div id="mixed-case"></div>',
-  //   errors: [
-  //     { message: "Template id attributes should be in kebab-case." },
-  //   ],
-  // },
-  // {
-  //   code: '<div id="Mixed case {{ withInterpolation }}"></div>',
-  //   output: '<div id="mixed-case-{{ withInterpolation }}"></div>',
-  //   errors: [
-  //     { message: "Template id attributes should be in kebab-case." },
-  //   ],
-  // },
+        {
+          code: '<div id="kebab-case "></div>',
+          output: '<div id="kebab-case"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id=" kebab-case "></div>',
+          output: '<div id="kebab-case"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="kebab-Case {{ withInterpolation }}"></div>',
+          output: '<div id="kebab-case-{{ withInterpolation }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="kebab-Case {{ withInterpolation | andPipe }}"></div>',
+          output:
+            '<div id="kebab-case-{{ withInterpolation | andPipe }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: `<div id="kebab-Case {{ withInterpolation | andPipe:arg1:argMethod(a1, a2, a3):'arg3' }}"></div>`,
+          output: `<div id="kebab-case-{{ withInterpolation | andPipe:arg1:argMethod(a1, a2, a3):'arg3' }}"></div>`,
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="kebab-case {{ withInterpolation }}"></div>',
+          output: '<div id="kebab-case-{{ withInterpolation }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="kebab-case {{ withInterpolation+andMore }}"></div>',
+          output: '<div id="kebab-case-{{ withInterpolation+andMore }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="snake_case"></div>',
+          output: '<div id="snake-case"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="snake_case_{{ withInterpolation }}"></div>',
+          output: '<div id="snake-case-{{ withInterpolation }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="snake_case_{{ myObject.withInterpolation }}"></div>',
+          output:
+            '<div id="snake-case-{{ myObject.withInterpolation }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="snake_case_{{ this.withInterpolation }}"></div>',
+          output: '<div id="snake-case-{{ this.withInterpolation }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="snake_case_{{ this.myObject.withInterpolation }}"></div>',
+          output:
+            '<div id="snake-case-{{ this.myObject.withInterpolation }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="snake_case_{{ withMethod() }}"></div>',
+          output: '<div id="snake-case-{{ withMethod() }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: `<div id="snake_case_{{ withVar + 'and_string' + withMethod(someArgument, this.someOther(index)) + { magna: 'carta', magnum: { opus: 1 } } + 512 + [1, 2, 3] + null + false + undefined }}"></div>`,
+          output: `<div id="snake-case-{{ withVar + 'and_string' + withMethod(someArgument, this.someOther(index)) + { magna: 'carta', magnum: { opus: 1 } } + 512 + [1, 2, 3] + null + false + undefined }}"></div>`,
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="snake_case_{{ withInterpolation + andMore }}"></div>',
+          output:
+            '<div id="snake-case-{{ withInterpolation + andMore }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="camelCase"></div>',
+          output: '<div id="camel-case"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="camelCase{{ withInterpolation }}"></div>',
+          output: '<div id="camel-case-{{ withInterpolation }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="PascalCase"></div>',
+          output: '<div id="pascal-case"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="PascalCase{{ withInterpolation }}"></div>',
+          output: '<div id="pascal-case-{{ withInterpolation }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="Mixed case"></div>',
+          output: '<div id="mixed-case"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
+        {
+          code: '<div id="Mixed case {{ withInterpolation }}"></div>',
+          output: '<div id="mixed-case-{{ withInterpolation }}"></div>',
+          options,
+          errors: [
+            { message: "Template id attributes should be in kebab-case." },
+          ],
+        },
       ],
     });
   });
@@ -186,7 +260,11 @@ describe("html-id-case", () => {
       invalid: [
         {
           code: '<div id="snake_case-{{ withInterpolation }}"></div>',
+          output: '<div id="snake_case_{{ withInterpolation }}"></div>',
           options,
+          errors: [
+            { message: "Template id attributes should be in snake_case." },
+          ],
         },
       ],
     });
@@ -217,7 +295,11 @@ describe("html-id-case", () => {
       invalid: [
         {
           code: '<div id="camelCase-{{ withInterpolation }}"></div>',
+          output: '<div id="camelCase{{ withInterpolation }}"></div>',
           options,
+          errors: [
+            { message: "Template id attributes should be in camelCase." },
+          ],
         },
       ],
     });
@@ -248,16 +330,20 @@ describe("html-id-case", () => {
       invalid: [
         {
           code: '<div id="PascalCase-{{ withInterpolation }}"></div>',
+          output: '<div id="PascalCase{{ withInterpolation }}"></div>',
           options,
+          errors: [
+            { message: "Template id attributes should be in PascalCase." },
+          ],
         },
       ],
     });
   });
 
-  describe("Mixed Case", () => {
+  describe("Other cases", () => {
     const options = [{ case: "kebab" }];
 
-    ruleTester.run("Mixed Case", rule, {
+    ruleTester.run("Miscellaneous", rule, {
       valid: [
         {
           code: '<div id="ignored Case {{ withInterpolation }}"></div>',
@@ -266,8 +352,19 @@ describe("html-id-case", () => {
       ],
       invalid: [
         {
+          code: '<div id=""></div>',
+          options,
+          errors: [{ message: "Template id attributes should not be empty." }],
+        },
+        {
+          code: '<div id=" "></div>',
+          options,
+          errors: [{ message: "Template id attributes should not be empty." }],
+        },
+        {
           code: '<div id="Some Mixed-Case"></div>',
           output: '<div id="some-mixed-case"></div>',
+          options,
           errors: [
             { message: "Template id attributes should be in kebab-case." },
           ],
@@ -276,6 +373,7 @@ describe("html-id-case", () => {
         {
           code: '<div id="Some Mixed-Case {{ withInterpolation }}"></div>',
           output: '<div id="some-mixed-case-{{ withInterpolation }}"></div>',
+          options,
           errors: [
             { message: "Template id attributes should be in kebab-case." },
           ],
