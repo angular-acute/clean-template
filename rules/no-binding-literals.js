1759b8a0c2b8aa9bda6891055d5ff500e620b7c7
Thomas Hillebrandt <thillebrandt@marginplug.in>
2024-12-31 15:32:42 -0600

    Improve handling of structural directives

--- rules/no-binding-literals.js
+++ rules/no-binding-literals.js
@@ -3,34 +3,57 @@ module.exports = {
     fixable: "code",
     type: "problem",
     messages: {
-      message: "Unnecessary binding of string literal {{ theString }}.",
+      literal: "Unnecessary binding of string literal {{ theString }}.",
+      structural:
+        "Pointless structural binding of string literal {{ theString }}.",
     },
   },
   create: (context) => {
-    const range = (node) => [
+    const range = (node,plus) => [
       node.sourceSpan.start.offset,
-      node.sourceSpan.end.offset,
+      node.sourceSpan.end.offset+(plus?1:0),
     ];
 
     return {
       BoundAttribute(node) {
-        if (
-          /^'[^']+'$/.test(node.value?.source) &&
-          !node.name.startsWith("ng")
+        if (/^'[^']+'$/.test(node.value?.source)) {
+          if (!node.name.startsWith("ng")) {
+            context.report({
+              node,
+              messageId: "literal",
+              data: { theString: node.value.source },
+              fix(fixer) {
+                return fixer.replaceTextRange(
+                  range(node),
+                  `${node.name}="${node.value.ast.value}"`
+                );
+              },
+            });
+          } else if (
+            !["ngSwitchCase", "ngSwitchDefault", "ngClass", "ngStyle"].includes(node.name)
+          ) {
+            context.report({
+              node,
+              messageId: "structural",
+              data: { theString: node.value.source },
+            });
+        } else if (
+           (node.name)==="ngSwitchDefault"
         ) {
           context.report({
             node,
-            messageId: "message",
+            messageId: "structural",
             data: { theString: node.value.source },
             fix(fixer) {
               return fixer.replaceTextRange(
-                range(node),
-                `${node.name}="${node.value.ast.value}"`
+                range(node,true),
+                `${node.name}`
               );
             },
-          });
+        });
         }
-      },
-    };
+      }
+    }
+          };
   },
 };
