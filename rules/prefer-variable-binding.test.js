ee352dcd3267c0b899dff0e360facb1239b808fe
Thomas Hillebrandt <thillebrandt@marginplug.in>
2024-12-31 18:19:45 -0600

    Tests

--- rules/prefer-variable-binding.test.js
+++ rules/prefer-variable-binding.test.js
@@ -1,6 +1,6 @@
 const { RuleTester } = require("eslint");
 const angularParser = require("@angular-eslint/template-parser");
-const rule = require("./no-binding-literals");
+const rule = require("./prefer-variable-binding");
 
 const ruleTester = new RuleTester({
   languageOptions: {
@@ -15,49 +15,124 @@ const ruleTester = new RuleTester({
   },
 });
 
-describe("no-binding-literals", () => {
-  ruleTester.run("kebab-case", rule, {
+describe("prefer-variable-binding", () => {
+  ruleTester.run("prefer-variable-binding", rule, {
     valid: [
       {
         name: `prop="my string"`,
         code: '<div prop="my string"></div>',
       },
       {
-        name: `[prop]="'my' + 'string'"`,
-        code: `<div [prop]="'my' + 'string'"></div>`,
-      },
-      {
-        name: `*ngSwitchCase="'my string'"`,
-        code: `<div *ngSwitchCase="'my string'"></div>`,
+        name: `prop="{{ var1 }} {{ var 2 }}"`,
+        code: `<div prop="{{ var1 }} {{ var 2 }}"></div>`,
       },
     ],
     invalid: [
       {
-        name: `[prop]="'my string'"`,
-        code: `<div [prop]="'my string'"></div>`,
-        output: '<div prop="my string"></div>',
-        errors: [
-          { message: "Unnecessary binding of string literal 'my string'." },
-        ],
-      },
-      {
-        name: `*ngIf="'bananas'"`,
-        code: `<div *ngIf="'bananas'"></div>`,
+        name: `prop="{{ myVariable }}"`,
+        code: `<div prop="{{ myVariable }}"></div>`,
+        output: '<div [prop]="myVariable"></div>',
         errors: [
           {
-            message:
-              "Pointless structural binding of string literal 'bananas'.",
+            message: "Prefer binding 'myVariable', rather than interpolating.",
           },
         ],
       },
       {
-        name: `*ngSwitchDefault="'bananas'"`,
-        code: `<div *ngSwitchDefault="'bananas'"></div>`,
-        output: "<div *ngSwitchDefault></div>",
+        name: `prop="{{ 'string' }}"`,
+        code: `<div prop="{{ 'string' }}"></div>`,
+        output: '<div prop="string"></div>',
+        errors: [
+          {
+            message: "Unnecessary interpolation of 'string'.",
+          },
+        ],
+      },
+      {
+        name: `prop="{{ 42 }}"`,
+        code: `<div prop="{{ 42 }}"></div>`,
+        output: '<div [prop]="42"></div>',
+        errors: [
+          {
+            message: "Unnecessary interpolation of '42'.",
+          },
+        ],
+      },
+      {
+        name: `prop="{{ myVariable + myOtherVariable }}"`,
+        code: `<div prop="{{ myVariable + myOtherVariable }}"></div>`,
+        output: '<div [prop]="myVariable + myOtherVariable"></div>',
         errors: [
           {
             message:
-              "Pointless structural binding of string literal 'bananas'.",
+              "Prefer binding 'myVariable + myOtherVariable', rather than interpolating.",
+          },
+        ],
+      },
+      {
+        name: `prop="{{ myMethod(index) }}"`,
+        code: `<div prop="{{ myMethod(index) }}"></div>`,
+        output: '<div [prop]="myMethod(index)"></div>',
+        errors: [
+          {
+            message:
+              "Prefer binding 'myMethod(index)', rather than interpolating.",
+          },
+        ],
+      },
+      {
+        name: `prop="{{ myVariable + myMethod(index) }}"`,
+        code: `<div prop="{{ myVariable + myMethod(index) }}"></div>`,
+        output: '<div [prop]="myVariable + myMethod(index)"></div>',
+        errors: [
+          {
+            message:
+              "Prefer binding 'myVariable + myMethod(index)', rather than interpolating.",
+          },
+        ],
+      },
+      {
+        name: `prop="{{\n  myVariable\n  + myOtherVariable\n}}"`,
+        code: `<div prop="{{
+          myVariable
+          + myOtherVariable
+        }}"></div>`,
+        output: `<div [prop]="
+          myVariable
+          + myOtherVariable
+        "></div>`,
+        errors: [
+          {
+            message:
+              "Prefer binding 'myVariable + myOtherVariable', rather than interpolating.",
+          },
+        ],
+      },
+      {
+        name: `prop="{{ \`\n  myVariable\n  + myOtherVariable\n\` }}"`,
+        code: `<div prop="{{ \`
+          myVariable
+          + myOtherVariable
+        \` }}"></div>`,
+        output: `<div [prop]="
+          myVariable
+          + myOtherVariable
+        "></div>`,
+        errors: [
+          {
+            message:
+              "Unnecessary interpolation of ' myVariable + myOtherVariable '.",
+          },
+        ],
+      },
+      {
+        name: `prop="{{ this.myVariable }}"`,
+        code: `<div prop="{{ this.myVariable }}"></div>`,
+        output: '<div [prop]="this.myVariable"></div>',
+        errors: [
+          {
+            message:
+              "Prefer binding 'this.myVariable', rather than interpolating.",
           },
         ],
       },
