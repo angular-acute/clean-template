c3116e9a0e22d75c648fc9091d846cf3751a829f
Thomas Hillebrandt <thillebrandt@marginplug.in>
2025-01-02 00:12:19 -0600

    Tests

--- rules/no-template-this.test.js
+++ rules/no-template-this.test.js
@@ -19,47 +19,78 @@ describe("no-template-this", () => {
   ruleTester.run("no-template-this", rule, {
     valid: [
       {
-        name: `prop="my string"`,
-        code: '<div prop="my string"></div>',
+        name: `[prop]="someVariable"`,
+        code: '<div [prop]="someVariable"></div>',
       },
       {
-        name: `[prop]="'my' + 'string'"`,
-        code: `<div [prop]="'my' + 'string'"></div>`,
+        name: `(prop)="someMethod()"`,
+        code: '<div (prop)="someMethod()"></div>',
       },
       {
-        name: `*ngSwitchCase="'my string'"`,
-        code: `<div *ngSwitchCase="'my string'"></div>`,
+        name: `(prop)="someMethod($event)"`,
+        code: '<div (prop)="someMethod($event)"></div>',
+      },
+      {
+        name: `(prop)="someMethod($event, stuff)"`,
+        code: '<div (prop)="someMethod($event, stuff)"></div>',
+      },
+      {
+        name: `[(prop)]="someVariable"`,
+        code: '<div [(prop)]="someVariable"></div>',
+      },
+      {
+        name: `[prop]="someVariable + someOtherVariable"`,
+        code: '<div [prop]="someVariable + someOtherVariable"></div>',
       },
     ],
     invalid: [
       {
-        name: `[prop]="'my string'"`,
-        code: `<div [prop]="'my string'"></div>`,
-        output: '<div prop="my string"></div>',
-        errors: [
-          { message: "Unnecessary binding of string literal 'my string'." },
-        ],
+        name: `[prop]="this.someVariable"`,
+        code: '<div [prop]="this.someVariable"></div>',
+        output: '<div [prop]="someVariable"></div>',
+        errors: [{ message: "Unnecessary `this.` in template." }],
       },
       {
-        name: `*ngIf="'bananas'"`,
-        code: `<div *ngIf="'bananas'"></div>`,
-        errors: [
-          {
-            message:
-              "Pointless structural binding of string literal 'bananas'.",
-          },
-        ],
+        name: `(prop)="this.someMethod()"`,
+        code: '<div (prop)="this.someMethod()"></div>',
+        output: '<div (prop)="someMethod()"></div>',
+        errors: [{ message: "Unnecessary `this.` in template." }],
       },
       {
-        name: `*ngSwitchDefault="'bananas'"`,
-        code: `<div *ngSwitchDefault="'bananas'"></div>`,
-        output: "<div *ngSwitchDefault></div>",
-        errors: [
-          {
-            message:
-              "Pointless structural binding of string literal 'bananas'.",
-          },
-        ],
+        name: `(prop)="this.someMethod($event)"`,
+        code: '<div (prop)="this.someMethod($event)"></div>',
+        output: '<div (prop)="someMethod($event)"></div>',
+        errors: [{ message: "Unnecessary `this.` in template." }],
+      },
+      {
+        name: `(prop)="this.someMethod($event, stuff)"`,
+        code: '<div (prop)="this.someMethod($event, stuff)"></div>',
+        output: '<div (prop)="someMethod($event, stuff)"></div>',
+        errors: [{ message: "Unnecessary `this.` in template." }],
+      },
+      {
+        name: `[(prop)]="this.someVariable"`,
+        code: '<div [(prop)]="this.someVariable"></div>',
+        output: '<div [(prop)]="someVariable"></div>',
+        errors: [{ message: "Unnecessary `this.` in template." }],
+      },
+      {
+        name: `[prop]="this.someVariable + someOtherVariable"`,
+        code: '<div [prop]="this.someVariable + someOtherVariable"></div>',
+        output: '<div [prop]="someVariable + someOtherVariable"></div>',
+        errors: [{ message: "Unnecessary `this.` in template." }],
+      },
+      {
+        name: `[prop]="someVariable + this.someOtherVariable"`,
+        code: '<div [prop]="someVariable + this.someOtherVariable"></div>',
+        output: '<div [prop]="someVariable + someOtherVariable"></div>',
+        errors: [{ message: "Unnecessary `this.` in template." }],
+      },
+      {
+        name: `[prop]="this.someVariable + this.someOtherVariable"`,
+        code: '<div [prop]="this.someVariable + this.someOtherVariable"></div>',
+        output: '<div [prop]="someVariable + someOtherVariable"></div>',
+        errors: [{ message: "Unnecessary `this.` in template." }],
       },
     ],
   });
