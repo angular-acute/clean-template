e4064ae3a306dccf3acf30fb83f9e58b41018b04
Thomas Hillebrandt <thillebrandt@marginplug.in>
2025-01-02 00:12:11 -0600

    Tidy up, better handling of this, reusable

--- rules/no-template-this.js
+++ rules/no-template-this.js
@@ -1,50 +1,66 @@
+const range = (value) => [
+  value.sourceSpan.start,
+  value.source.includes(" =$event") // Two-way bindings need the end truncated
+    ? value.sourceSpan.end - 8
+    : value.sourceSpan.end,
+];
+
+const finder = /([^\w]|^)this\./;
+const replacer = /([^\w]|^)this\./g;
+
+const hasThis = (strng) => {
+  if (!strng) return false;
+
+  return !!finder.exec(strng);
+};
+
+function handleContext(context, node, replaceRange, replaceText) {
+  context.report({
+    node,
+    messageId: "this",
+    fix(fixer) {
+      return fixer.replaceTextRange(replaceRange, replaceText);
+    },
+  });
+}
+
 module.exports = {
-  meta: { 
-    fixable: 'code'
-    , type: 'suggestion' , messages: {
-       this:'Unnecessary `this.` in template.'
+  meta: {
+    fixable: "code",
+    type: "suggestion",
+    messages: {
+      this: "Unnecessary `this.` in template.",
     },
     docs: {
-      description:
-        "Help prevent using unnecessary `this` in templates.",
+      description: "Help prevent using unnecessary `this` in templates.",
       url: "",
     },
   },
   create: (context) => {
-    const range = (value) => [
-      value.sourceSpan.start,
-      value.source.includes(' =$event') // Two-way bindings need the end truncated
-        ? value.sourceSpan.end - 8
-        : value.sourceSpan.end,
-    ];
+    let isHandled = false;
 
     return {
       BoundAttribute(node) {
-        if (node.value?.source?.startsWith('this.')) {
-          context.report({
+        if (hasThis(node.value?.source) && !isHandled) {
+          isHandled = true;
+          handleContext(
+            context,
             node,
-            messageId: 'this',
-            fix(fixer) {
-              return fixer.replaceTextRange(
-                range(node.value),
-                node.value.source.replace('this.', '')
-              );
-            },
-          });
+            range(node.value),
+            node.value.source.replace(replacer, "$1")
+          );
         }
       },
       BoundEvent(node) {
-        if (node.handler?.source?.startsWith('this.')) {
-          context.report({
+        if (hasThis(node.handler?.source) && !isHandled) {
+          isHandled = true;
+          handleContext(
+            context,
             node,
-            messageId: 'this',
-            fix(fixer) {
-              return fixer.replaceTextRange(
-                range(node.handler),
-                node.handler.source.replace('this.', '').replace(' =$event', '') // The last .replace is necessary for two-way bindings
-              );
-            },
-          });
+            range(node.handler),
+            /* The last .replace is necessary for two-way bindings */
+            node.handler.source.replace(replacer, "$1").replace(" =$event", "")
+          );
         }
       },
     };
